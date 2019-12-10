c56edfbbea3b572d299e96c96a21eb50efbb1e21
morlinski <michal.orlinski@whirly.pl>
2019-12-10 18:46:08 +0100

    Generate Spring Boot project with JTwig dependency

---
+++ src/main/java/tool/JtwigPageVisitCounterApplication.java
@@ -0,0 +1,13 @@
+package tool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JtwigPageVisitCounterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JtwigPageVisitCounterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tool/JtwigPageVisitCounterApplicationTests.java
@@ -0,0 +1,13 @@
+package tool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JtwigPageVisitCounterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
